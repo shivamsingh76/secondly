2e21db79f2a23f139e79ec888c0bb98a35567872
Shivam Singh <shivam.singh0706@gmail.com>
2025-11-18 20:40:46 +0530

    SafeArea widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,21 +11,23 @@ class MyWidget extends StatelessWidget {
   Widget build(BuildContext context) {
      return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Center(child: Text('Jhunnu App')),
-          backgroundColor: const Color.fromARGB(255, 199, 67, 85),
-        ),
-        body: Container(
-          color: Colors.lightGreen[200],
-          width: 150.0,
-          height: 150.0,
-          padding: const EdgeInsets.all(20.0),
-          alignment: Alignment.center,
-          margin: const EdgeInsets.all(100.0),
-          transform: Matrix4.rotationZ(0.1),
-          child: Text(
-            'Hello, Jhunnu!',
-            style: TextStyle(fontSize: 24),
+        // appBar: AppBar(
+        //   title: const Center(child: Text('Jhunnu App')),
+        //   backgroundColor: const Color.fromARGB(255, 199, 67, 85),
+        // ),
+        body: SafeArea(
+          child: Container(
+            color: Colors.lightGreen[200],
+            width: 150.0,
+            height: 150.0,
+            padding: const EdgeInsets.all(20.0),
+            alignment: Alignment.center,
+            // margin: const EdgeInsets.all(100.0),
+            transform: Matrix4.rotationZ(0.1),
+            child: Text(
+              'Hello, Jhunnu!',
+              style: TextStyle(fontSize: 24),
+            ),
           ),
         ), 
       ),
